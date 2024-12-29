3beb63c686c71f7a26f2df089f767233807c093b
arya15008 <aryaprasetyawan880.com>
2024-12-29 19:05:57 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="card-body">
               <div class="text-center mb-3">
                 <i class="bi bi-person-circle h1 display-4"></i>
-                <p>Login Account</p>
+                <p>Welcome to my daily journal</p>
                 <hr />
               </div>
               <form action="" method="post">
